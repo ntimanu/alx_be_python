828b00f5969343b8fca7d93d522ae95215952a4e
ntimanu <ntihinyukae@gmail.com>
2024-12-18 12:52:02 +0200

    Add basic arthmetic exercise file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+mult = number1 * number2
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", sub)
+print("Multiplication of", number1, "and", number2, "is", mult)
