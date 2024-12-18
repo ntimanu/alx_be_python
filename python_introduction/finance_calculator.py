612ed8fe68a8913453f2821022267e946744ccee
ntimanu <ntihinyukae@gmail.com>
2024-12-18 20:21:53 +0200

    Fix convesion error from int to float and update file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = int(input("What is your monthly income"))
-monthly_expences = int(input("Enter yout total monthly expenses"))
+monthly_income = float(input("What is your monthly income: "))
+monthly_expences = float(input("Enter yout total monthly expenses: "))
 monthly_savings = monthly_income - monthly_expences
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print("Your monthly savings are $",monthly_savings)
-print("Projected savings after one year, with interest, is:", projected_savings)
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
