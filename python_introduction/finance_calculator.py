e50cd5a3ade85c301fd777f75a47041e07b6e743
ntimanu <ntihinyukae@gmail.com>
2024-12-18 20:04:58 +0200

    Fix convetor error finance_calculator file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,6 @@
-monthly_income = input("What is your monthly income")
-monthly_expences = input("Enter yout total monthly expenses")
+monthly_income = int(input("What is your monthly income"))
+monthly_expences = int(input("Enter yout total monthly expenses"))
 monthly_savings = monthly_income - monthly_expences
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print("Your monthly savings are $",monthly_savings)
 print("Projected savings after one year, with interest, is:", projected_savings)
