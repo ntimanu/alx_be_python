74326acd01015d9ed4bfdddfa07982ecb7723054
ntimanu <ntihinyukae@gmail.com>
2025-01-07 22:25:31 +0200

    Fix if statment text

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,19 +1,19 @@
 task = input("Enter your task:")
 priority = input("Priority (high/medium/low):").strip().lower()
-time = input("Is it time-bound? (yes/no):").strip().lower()
+time_bound = input("Is it time-bound? (yes/no):").strip().lower()
 match priority:
     case "high":
-        if time == "yes":
+        if time_bound == "yes":
             print(f"{task} is a high priority task that requires immediate attention today!")
         else:
             print(f"{task} is a low priority task. consider completing it when you have free time.")
     case "medium":
-        if time == "yes":
+        if time_bound == "yes":
             print(f"{task} is a high priority task that requires immediate attention today!")
         else:
             print(f"{task} is a low priority task. consider completing it when you have free time.")
     case "low":
-        if time == "yes":
+        if time_bound == "yes":
             print(f"{task} is a high priority task that requires immediate attention today!")
         else:
             print(f"{task} is a low priority task. consider completing it when you have free time.")
