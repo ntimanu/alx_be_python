f10234ea3f5430ce254c9ed5712693ba433514d9
ntimanu <ntihinyukae@gmail.com>
2025-01-07 20:09:08 +0200

    Fix input text

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 # Prompt the user for input
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-operation = input("Choose the operation: +, -, *, /: ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
 
 # Initialize result
 result = None
